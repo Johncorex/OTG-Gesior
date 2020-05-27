c05978ff55c55e213139ea998b1fde8e56f73298
Johncore <gjohnes2018@gmail.com>
2020-05-27 11:14:44 -0300

    grupo otg

--- layouts/tibiacom/layout.php
+++ layouts/tibiacom/layout.php
@@ -1,3 +1,14 @@
+<style>
+	.zaptable{position:absolute;margin-left:65%;text-align:left;background:#01010185;border:2px solid #8595bb;padding:9px 8px 6px 47px;border-radius:0 0 10px 10px;color:#fff;margin-top:-2px;font:400 9pt Verdana,Arial,Times New Roman,sans-serif;box-shadow:6px 6px 50px 2px #23d100;z-index:10000;transition:all 600ms ease-out}.zaptable:hover{box-shadow:2px 2px 18px 1px #03ff0d;border:2px solid #03ff0d;background:#343f58;transition:all 500ms ease-out;z-index:10000;cursor:pointer}.zaptable img{position:absolute;margin-left:-47px;margin-top:-8px;width:43px}.zaptable small{transition:all 500ms ease-out;display:none}.zaptable:hover small{transition:all 500ms ease-out;display:block}
+	</style>
+	<a href="" target="_blank">
+		<div class="zaptable">
+			<div class="zapbox">
+				<img src="/images/whats.png"><strong>Grupo Whatsapp: </strong>OTG-Global<br>
+				<strong>Máximo de pessoas: </strong> 250 <small style="color: #00c51d;text-shadow: 1px 1px black;text-align:center;">Clique aqui para entrar!!</small>
+			</div>
+		</div>
+	</a>
 <?php
 if(!defined('INITIALIZED'))
     exit;
