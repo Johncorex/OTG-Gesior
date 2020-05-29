15c85e7fbcc2a5ebd3d3778e9fda8ff78ca9fc72
Johncore <gjohnes2018@gmail.com>
2020-05-29 13:47:45 -0300

    Update antisql.php

--- antisql.php
+++ antisql.php
@@ -1,4 +1,3 @@
-<!-- Credits Johncore -->
 <?php
 $xa = getenv('REMOTE_ADDR');
 $badwords = array(";","'","\"","*","union","x:","x:\#","delete ","///","INSERT","DROP","/","DROP TABLE","UPDATE Character SET","UPDATE players SET","UPDATE account SET","UPDATE CHARACTER SET","UPDATE PLAYERS SET","UPDATE ACCOUNT SET","UPDATE","WHERE","SET","ADMIN","GOD","ADM","from|xp_|execute|exec|sp_executesql|sp_|select|update|set|shutdown|insert|delete|where|drop table|show tables|#|\*|","DELETE","insert",","|"x'; U\PDATE Character S\ET level=99;-\-","x';U\PDATE Account S\ET ugradeid=255;-\-","x';U\PDATE Account D\ROP ugradeid=255;-\-","x';U\PDATE Account D\ROP ",",W\\HERE 1=1;-\\-","z'; U\PDATE Account S\ET ugradeid=char","update","drop","sele","memb","set" ,"$","res3t","wareh","%","--"); 
